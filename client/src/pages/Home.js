f536af4a9605dd0ef3886d2b9792b585b2f9fb95
Salman <salmaanmohamed700@gmail.com>
2025-06-19 20:35:01 +0300

    Added homepage

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Startup Simulator</h1>
+    <p>Register or log in to start managing your startups and investments.</p>
+  </div>
+);
+
+export default Home;
