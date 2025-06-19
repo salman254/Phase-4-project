4185ae264ecc27fca6016280487818fb61e32ee7
Salman <salmaanmohamed700@gmail.com>
2025-06-19 15:14:14 +0300

    Added tables

---
+++ server/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from server.extensions import db, bcrypt
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    startups = db.relationship('Startup', backref='owner', lazy=True)
+    investments = db.relationship('Investment', backref='investor', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode()
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+class Startup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50))
+    funding_goal = db.Column(db.Float)
+    current_funding = db.Column(db.Float, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    investments = db.relationship('Investment', backref='startup', lazy=True)
+
+class Investment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    startup_id = db.Column(db.Integer, db.ForeignKey('startup.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date_invested = db.Column(db.DateTime, default=datetime.utcnow)
+
