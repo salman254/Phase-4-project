f5507e1ba06e26a1b2754791e3c79e5b994c6249
Salman <salmaanmohamed700@gmail.com>
2025-06-19 15:16:00 +0300

    Added user routes

---
+++ server/auth/routes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from server.models import User
+from server.extensions import db
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    user = User(username=data['username'], email=data['email'])
+    user.set_password(data['password'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(message='User registered'), 201
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(email=data['email']).first()
+    if user and user.check_password(data['password']):
+        token = create_access_token(identity=user.id)
+        return jsonify(access_token=token)
+    return jsonify(error='Invalid credentials'), 401
+
+@auth_bp.route('/me', methods=['GET'])
+@jwt_required()
+def get_profile():
+    user = User.query.get(get_jwt_identity())
+    return jsonify(username=user.username, email=user.email, is_admin=user.is_admin)
+
