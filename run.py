47f963ebabee87b892733f0862d22dd2b1044a2c
Salman <salmaanmohamed700@gmail.com>
2025-06-19 20:31:55 +0300

    Run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from server.app import create_app
+from server import create_app
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
