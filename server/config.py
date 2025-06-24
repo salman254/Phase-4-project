da62a172ae4d2977cbb18b5908e75f2bf7a958cd
Salman <salmaanmohamed700@gmail.com>
2025-06-24 13:45:48 +0300

    added the path for the uploaded folder

--- server/config.py
+++ server/config.py
@@ -5,3 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(basedir, 'app.db')}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret')
+    UPLOAD_FOLDER = os.path.join(basedir, 'static/uploads')
