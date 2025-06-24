2bf09d968b410cb423e5b836db4b055fce529fd2
Salman <salmaanmohamed700@gmail.com>
2025-06-24 13:46:29 +0300

    added profile image in the user

--- server/models.py
+++ server/models.py
@@ -7,6 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    profile_image = db.Column(db.String(255), nullable=True) 
+
     startups = db.relationship('Startup', backref='owner', lazy=True)
     investments = db.relationship('Investment', backref='investor', lazy=True)
 
@@ -31,4 +33,3 @@ class Investment(db.Model):
     startup_id = db.Column(db.Integer, db.ForeignKey('startup.id'), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     date_invested = db.Column(db.DateTime, default=datetime.utcnow)
-
