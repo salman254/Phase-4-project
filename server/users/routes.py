5d25a1048fe0a7d1f41fc54bde2d38675e373336
Salman <salmaanmohamed700@gmail.com>
2025-06-24 13:47:56 +0300

    added profile image route

--- server/users/routes.py
+++ server/users/routes.py
@@ -1,5 +1,7 @@
+import os
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from werkzeug.utils import secure_filename
 from server.extensions import db
 from server.models import User, Startup
 
@@ -16,6 +18,31 @@ def update_profile():
         current_user.set_password(data['password'])
     db.session.commit()
     return jsonify(message='Profile updated')
+
+@users_bp.route('/profile-image', methods=['POST'])
+@jwt_required()
+def upload_profile_image():
+    user = User.query.get(get_jwt_identity())
+
+    if 'profile_image' not in request.files:
+        return jsonify(error='No file uploaded'), 400
+
+    file = request.files['profile_image']
+    if file.filename == '':
+        return jsonify(error='Empty filename'), 400
+
+    # Secure filename and save to uploads folder
+    filename = secure_filename(f"user_{user.id}_{file.filename}")
+    upload_folder = os.path.join('server', 'static', 'uploads')
+    os.makedirs(upload_folder, exist_ok=True)
+    path = os.path.join(upload_folder, filename)
+    file.save(path)
+
+    # Update user profile image in DB
+    user.profile_image = filename
+    db.session.commit()
+
+    return jsonify(message='Image uploaded', profile_image=filename)
 
 @users_bp.route('/admin/startups', methods=['GET'])
 @jwt_required()
@@ -79,7 +106,8 @@ def admin_dashboard():
                 "id": u.id,
                 "username": u.username,
                 "email": u.email,
-                "is_admin": u.is_admin
+                "is_admin": u.is_admin,
+                "profile_image": u.profile_image
             } for u in users
         ]
     })
